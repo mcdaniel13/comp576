610e6044b1d58157ea122f91a3e663103d550f37
Moongee Cho <35756853+mcdaniel13@users.noreply.github.com>
2022-09-30 11:38:52 -0500

    Update dcn_mnist-1.py

--- assignment1/dcn_mnist-1.py
+++ assignment1/dcn_mnist-1.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 if(tf.__version__.split('.')[0]=='2'):
     import tensorflow.compat.v1 as tf
-    tf.disable_v2_behavior()    
+    tf.disable_v2_behavior()
 
 # Load MNIST dataset
 import input_data
@@ -26,8 +26,8 @@ def weight_variable(shape):
     '''
 
     # IMPLEMENT YOUR WEIGHT_VARIABLE HERE
-
-    return W
+    initial = tf.truncated_normal(shape=shape, stddev=0.1)
+    return tf.Variable(initial)
 
 def bias_variable(shape):
     '''
@@ -38,8 +38,8 @@ def bias_variable(shape):
     '''
 
     # IMPLEMENT YOUR BIAS_VARIABLE HERE
-
-    return b
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
 
 def conv2d(x, W):
     '''
@@ -58,8 +58,7 @@ def conv2d(x, W):
     '''
 
     # IMPLEMENT YOUR CONV2D HERE
-
-    return h_conv
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 def max_pool_2x2(x):
     '''
@@ -69,8 +68,18 @@ def max_pool_2x2(x):
     '''
 
     # IMPLEMENT YOUR MAX_POOL_2X2 HERE
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-    return h_max
+def variable_summaries(var):
+  with tf.name_scope('summaries'):
+    mean = tf.reduce_mean(var)
+    tf.summary.scalar('mean', mean)
+    with tf.name_scope('stddev'):
+      stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+    tf.summary.scalar('stddev', stddev)
+    tf.summary.scalar('max', tf.reduce_max(var))
+    tf.summary.scalar('min', tf.reduce_min(var))
+    tf.summary.histogram('histogram', var)
 
 def main():
     # Specify training parameters
@@ -82,46 +91,46 @@ def main():
     # FILL IN THE CODE BELOW TO BUILD YOUR NETWORK
 
     # placeholders for input data and input labeles
-    x =
-    y_ =
+    x = tf.placeholder(tf.float32, [None, 784], name='x')
+    y_ = tf.placeholder(tf.float32, [None, 10], name='y_')
 
     # reshape the input image
-    x_image = tf.reshape(x, [-1, 28, 28, 1]
+    x_image = tf.reshape(x, [-1, 28, 28, 1])
 
     # first convolutional layer
-    W_conv1 =
-    b_conv1 =
-    h_conv1 =
-    h_pool1 =
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
 
     # second convolutional layer
-    W_conv2 =
-    b_conv2 =
-    h_conv2 =
-    h_pool2 =
+    W_conv2 = weight_variable([5, 5, 32, 64])
+    b_conv2 = bias_variable([64])
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
 
     # densely connected layer
-    W_fc1 =
-    b_fc1 =
-    h_pool2_flat =
-    h_fc1 =
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     # dropout
-    keep_prob =
-    h_fc1_drop =
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
     # softmax
-    W_fc2 =
-    b_fc2 =
-    y_conv =
+    W_fc2 = weight_variable([1024, 10])
+    b_fc2 = bias_variable([10])
+    y = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2, name='y')
 
     # FILL IN THE FOLLOWING CODE TO SET UP THE TRAINING
 
     # setup training
-    cross_entropy =
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    correct_prediction =
-    accuracy =
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
 
     # Add a scalar summary for the snapshot loss.
     tf.summary.scalar(cross_entropy.op.name, cross_entropy)
